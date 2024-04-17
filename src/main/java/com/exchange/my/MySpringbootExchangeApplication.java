e45023afab5343957b48ae457720172ad05b47a0
研发部220627202温陆城 <220627202@dl.com>
2024-04-17 15:48:17 +0800

    first commit

---
+++ src/main/java/com/exchange/my/MySpringbootExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.exchange.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringbootExchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringbootExchangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exchange/my/MySpringbootExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exchange.my;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringbootExchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
